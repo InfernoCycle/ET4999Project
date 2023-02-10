2a60e7c8b1eceed9e5bb10dee3aae9330c492938
Cornelius <cdrogers19sl@gmail.com>
2023-02-10 14:31:11 -0500

    2/10/2023 commit

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
 <?php 
-    echo"78999";
+    $server = "sql101.epizy.com";
+    $username = "epiz_33561916";
+    $password = "SNlJj0hLjrq";
+    $database = "epiz_33561916_MainDatabase";
+
+    $err = "";
+    $conn = null;
+    try{
+        $conn = mysqli_connect($server, $username, $password, $database);
+    }catch (Exception $e){
+        $err = $e;
+    }
+    
+
+    if(!$conn){
+        die("Connection Error: " . mysqli_connect_error());
+    }
+    //$conn->close();
 ?>
+
+<html>
+<body>
+
+Thank You <?php echo $_GET["fn"] . " " . $_GET["ln"]; ?><br>
+Your seat has been reserved at <?php echo $_GET["time"]; ?>
+ on <?php echo $_GET["date"];?>
+
+ <?php echo $e?>
+
+ <button onclick="home()">HOME</button>
+
+<script>
+    function home(){
+        window.location.replace("./index.html");
+    }
+</script>
+</body>
+</html>
